97abe5b2f773ac37fc34f92d4ccce0ecab87cf80
sunxiaoshen <sunxiaoshen@threatbook.cn>
2018-12-04 18:36:23 +0800

    fix

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# csdn-unwind
+# csdn-eye
+自动展开CSDN全文，无需手动点击阅读全文按钮
---
+++ src/js/content-script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function()
+{
+    var nd = document.getElementById('article_content');
+    nd.style.height = 'unset';
+    nd.style.overflow = 'unset';
+    document.querySelector('.hide-article-box').style.opacity = 0;
+});
+
